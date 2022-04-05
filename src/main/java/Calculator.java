dcd44f9c871a2788e0fcaf2eb07c32dc8c2b6143
leno <453853482@qq.com>
2022-04-05 08:38:33 +0800

    add calculator test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    public int compute(int num1, int num2, String symbol) {
+
+
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Test
+    public void shouldReturn2When1Plus1() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1, 1, "+");
+        int expectedResult = 2;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void shouldReturn0When1Sub1() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1, 1, "-");
+        int expectedResult = 0;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+}
