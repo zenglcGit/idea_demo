798afe72b64019a2b3218d816becae688345701e
leno <453853482@qq.com>
2022-04-05 08:43:18 +0800

    add calculator code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,12 @@
 public class Calculator {
-    public int compute(int num1, int num2, String symbol) {
-
-
+    public int compute(int num1, int num2, char symbol) {
+        switch (symbol) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
     @Test
     public void shouldReturn2When1Plus1() {
         Calculator calculator = new Calculator();
-        int actualResult = calculator.compute(1, 1, "+");
+        int actualResult = calculator.compute(1, 1, '+');
         int expectedResult = 2;
         Assert.assertEquals(expectedResult, actualResult);
     }
@@ -15,7 +15,7 @@ public class CalculatorTest {
     @Test
     public void shouldReturn0When1Sub1() {
         Calculator calculator = new Calculator();
-        int actualResult = calculator.compute(1, 1, "-");
+        int actualResult = calculator.compute(1, 1, '-');
         int expectedResult = 0;
         Assert.assertEquals(expectedResult, actualResult);
     }
